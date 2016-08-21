ed01440aa9ed2fcb5ee0a1e02d93fff6e8f1830b
Benjamin Franzi <benjamin.franzi@outlook.com>
2016-08-21 23:38:06 +1000

    client talks to server talks to mongo talks to me

--- src/client.py
+++ src/client.py
@@ -1,13 +1,15 @@
 import os
 from socket import *
+meId = "ABCDEF"
 host = "127.0.0.1" # set to IP address of target computer
 port = 13000
 addr = (host, port)
 UDPSock = socket(AF_INET, SOCK_DGRAM)
 while True:
-    data = raw_input("Enter message to send or type 'exit': ")
+    data = raw_input("enter bay count: ")
+    data = "" + data + meId
     UDPSock.sendto(data, addr)
-    if data == "exit":
+    if data == ("exit:" + meId):
         break
 UDPSock.close()
 os._exit(0)
